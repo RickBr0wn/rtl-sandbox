965a09896be2b2fbf8eb8ac63bf46ed63de96228
Rick Brown <ricky.brown.00@gmail.com>
2019-10-29 12:49:16 +0000

    Removed unnecessary imports

--- src/Header.test.js
+++ src/Header.test.js
@@ -1,14 +1,6 @@
 import React from 'react'
-import {
-  render,
-  cleanup,
-  fireEvent,
-  waitForElement
-} from '@testing-library/react'
+import { render, cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
-// the axios mock is in __mocks__/
-// see https://jestjs.io/docs/en/manual-mocks
-// import axiosMock from 'axios'
 import Header from './Header'
 
 afterEach(cleanup)
