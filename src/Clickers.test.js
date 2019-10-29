02331bab08da7147887a4bff97174f2bc48b0c19
Rick Brown <ricky.brown.00@gmail.com>
2019-10-29 13:33:43 +0000

    Added Clickers.js & Clickers.test.js

---
+++ src/Clickers.test.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import {
+  render,
+  cleanup,
+  fireEvent,
+  waitForElement
+} from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+// the axios mock is in __mocks__/
+// see https://jestjs.io/docs/en/manual-mocks
+// import axiosMock from 'axios'
+import Clickers from './Clickers'
+
+afterEach(cleanup)
+
+it('should display the count', () => {
+  const { getByTestId } = render(<Clickers />)
+  expect(getByTestId('count')).toHaveTextContent('0')
+})
+
+it('should increment the counter when the `UP` button is clicked', () => {
+  const { getByText, getByTestId } = render(<Clickers />)
+  fireEvent.click(getByText('Up'))
+  expect(getByTestId('count')).toHaveTextContent('1')
+})
+
+it('should asynchronously decrement the counter when the `DOWN` button is clicked', async () => {
+  const { getByText } = render(<Clickers />)
+  fireEvent.click(getByText('Down'))
+  const counterSpan = await waitForElement(() => getByText('-1'))
+  expect(counterSpan).toHaveTextContent('-1')
+})
