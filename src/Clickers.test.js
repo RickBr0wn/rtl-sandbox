00413ea6a1cdfb7412d76a1af5cfae91ccd6ce2a
Rick Brown <ricky.brown.00@gmail.com>
2019-11-27 22:20:52 +0000

    remove files not required for branch

--- src/Clickers.test.js
+++ src/Clickers.test.js
@@ -1,32 +1,29 @@
-import React from 'react'
+import React from 'react';
 import {
   render,
   cleanup,
   fireEvent,
-  waitForElement
-} from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
-// the axios mock is in __mocks__/
-// see https://jestjs.io/docs/en/manual-mocks
-// import axiosMock from 'axios'
-import Clickers from './Clickers'
+  waitForElement,
+} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Clickers from './Clickers';
 
-afterEach(cleanup)
+afterEach(cleanup);
 
 it('should display the count', () => {
-  const { getByTestId } = render(<Clickers />)
-  expect(getByTestId('count')).toHaveTextContent('0')
-})
+  const { getByTestId } = render(<Clickers />);
+  expect(getByTestId('count')).toHaveTextContent('0');
+});
 
 it('should increment the counter when the `UP` button is clicked', () => {
-  const { getByText, getByTestId } = render(<Clickers />)
-  fireEvent.click(getByText('Up'))
-  expect(getByTestId('count')).toHaveTextContent('1')
-})
+  const { getByText, getByTestId } = render(<Clickers />);
+  fireEvent.click(getByText('Up'));
+  expect(getByTestId('count')).toHaveTextContent('1');
+});
 
-// it('should asynchronously decrement the counter when the `DOWN` button is clicked', async () => {
-//   const { getByText } = render(<Clickers />)
-//   fireEvent.click(getByText('Down'))
-//   const counterSpan = await waitForElement(() => getByText('-1'))
-//   expect(counterSpan).toHaveTextContent('-1')
-// })
+it('should asynchronously decrement the counter when the `DOWN` button is clicked', async () => {
+  const { getByText } = render(<Clickers />);
+  fireEvent.click(getByText('Down'));
+  const counterSpan = await waitForElement(() => getByText('-1'));
+  expect(counterSpan).toHaveTextContent('-1');
+});
