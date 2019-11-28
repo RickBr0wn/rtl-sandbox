6b168011d074d1be1e847dee8511d744aa6098c1
Rick Brown <ricky.brown.00@gmail.com>
2019-11-28 00:28:41 +0000

    accepted changes

--- src/Clickers.test.js
+++ src/Clickers.test.js
@@ -1,43 +1,29 @@
-import React from 'react';
+import React from 'react'
 import {
   render,
   cleanup,
   fireEvent,
   waitForElement,
-<<<<<<< HEAD
 } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import Clickers from './Clickers'
-=======
-} from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import Clickers from './Clickers';
->>>>>>> f0588adf2f7fdce5544f91e3a1776f156fbd9ddf
 
-afterEach(cleanup);
+afterEach(cleanup)
 
 it('should display the count', () => {
-  const { getByTestId } = render(<Clickers />);
-  expect(getByTestId('count')).toHaveTextContent('0');
-});
+  const { getByTestId } = render(<Clickers />)
+  expect(getByTestId('count')).toHaveTextContent('0')
+})
 
 it('should increment the counter when the `UP` button is clicked', () => {
-  const { getByText, getByTestId } = render(<Clickers />);
-  fireEvent.click(getByText('Up'));
-  expect(getByTestId('count')).toHaveTextContent('1');
-});
+  const { getByText, getByTestId } = render(<Clickers />)
+  fireEvent.click(getByText('Up'))
+  expect(getByTestId('count')).toHaveTextContent('1')
+})
 
 it('should asynchronously decrement the counter when the `DOWN` button is clicked', async () => {
-<<<<<<< HEAD
   const { getByText } = render(<Clickers />)
   fireEvent.click(getByText('Down'))
   const counterSpan = await waitForElement(() => getByText('-1'))
   expect(counterSpan).toHaveTextContent('-1')
 })
-=======
-  const { getByText } = render(<Clickers />);
-  fireEvent.click(getByText('Down'));
-  const counterSpan = await waitForElement(() => getByText('-1'));
-  expect(counterSpan).toHaveTextContent('-1');
-});
->>>>>>> f0588adf2f7fdce5544f91e3a1776f156fbd9ddf
